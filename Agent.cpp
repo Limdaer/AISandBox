9e20bde073010c13a93ff0f8365ea7e0e6082f9a
Limdaer <stanekondra01@gmail.com>
2025-12-31 17:28:09 +0100

    Fixed agent2 decision making

--- Agent.cpp
+++ Agent.cpp
@@ -14,22 +14,26 @@ void Agent::Update(Vector2 targetPos, World &world)
 	bool canMoveRight = world.testRight(*this);
 	bool canMoveUp = world.testUp(*this);
 	bool canMoveDown = world.testDown(*this);
+    float epsilon = 0.5f;
+    float directionx = targetPosition.x - position.x;
+	float directiony = targetPosition.y - position.y;
+    float length = sqrt(directionx * directionx + directiony * directiony);
+    if (length > 0.0f)
+    {
+        directionx /= length;
+        directiony /= length;
 
-    if(targetPosition.x >= position.x && canMoveRight)
-    {
-        position.x += speed;
-    }
-    else if(targetPosition.x <= position.x && canMoveLeft)
-    {
-        position.x -= speed;
-	}
-    if(targetPosition.y >= position.y && canMoveDown)
-    {
-        position.y += speed;
-    }
-    else if (targetPosition.y <= position.y && canMoveUp)
-    {
-        position.y -= speed;
+        // test pohybu X
+        if (directionx > 0 && canMoveRight)
+            position.x += directionx * speed;
+        else if (directionx < 0 && canMoveLeft)
+            position.x += directionx * speed;
+
+        // test pohybu Y
+        if (directiony > 0 && canMoveDown)
+            position.y += directiony * speed;
+        else if (directiony < 0 && canMoveUp)
+            position.y += directiony * speed;
     }
 }
 
@@ -38,16 +42,25 @@ void Agent::Update2(World& world)
 
 
     std::vector<Direction> possibleDirections;
-    if (world.testRight(*this) && position.x + speed < 700) possibleDirections.push_back(Direction::RIGHT);
-    if (world.testLeft(*this) && position.x - speed > 100) possibleDirections.push_back(Direction::LEFT);
-    if (world.testUp(*this) && position.y - speed > 100) possibleDirections.push_back(Direction::UP);
-    if (world.testDown(*this) && position.y + speed < 500) possibleDirections.push_back(Direction::DOWN);
-
-    bool lastIsPossible = std::find(possibleDirections.begin(), possibleDirections.end(), lastDirection) != possibleDirections.end();
-    if (!possibleDirections.empty() && changeDirectionTimer <= 0.0f || !lastIsPossible )
+    if (world.testRight(*this) && position.x + speed < 800 - size) possibleDirections.push_back(Direction::RIGHT);
+    if (world.testLeft(*this) && position.x - speed > 0 + size) possibleDirections.push_back(Direction::LEFT);
+    if (world.testUp(*this) && position.y - speed > 0 + size) possibleDirections.push_back(Direction::UP);
+    if (world.testDown(*this) && position.y + speed < 600 - size) possibleDirections.push_back(Direction::DOWN);
+    
+    bool lastIsPossible = std::find(possibleDirections.begin(), possibleDirections.end(), lastDirection) 
+        != possibleDirections.end(); if (!possibleDirections.empty() && changeDirectionTimer <= 0.0f || !lastIsPossible)
+    if (!possibleDirections.empty() && (changeDirectionTimer <= 0.0f || !lastIsPossible))
     {
+        // choose new direction every time
+		if (possibleDirections.size() > 1)
+            possibleDirections.erase(
+                std::remove(possibleDirections.begin(), possibleDirections.end(), lastDirection),
+                possibleDirections.end()
+            );
+
         int idx = GetRandomValue(0, possibleDirections.size() - 1);
         currentDirection = possibleDirections[idx];
+        changeDirectionTimer = 1.0f;
     }
     else
 		currentDirection = lastDirection;
@@ -55,26 +68,22 @@ void Agent::Update2(World& world)
     {
     case Direction::RIGHT:
 		lastDirection = Direction::RIGHT;
-		changeDirectionTimer = 10.0f;
         position.x += speed;
         break;
     case Direction::LEFT:
 		lastDirection = Direction::LEFT;
-		changeDirectionTimer = 10.0f;
         position.x -= speed;
         break;
     case Direction::UP:
 		lastDirection = Direction::UP;
-		changeDirectionTimer = 10.0f;
         position.y -= speed;
         break;
     case Direction::DOWN:
 		lastDirection = Direction::DOWN;
-		changeDirectionTimer = 10.0f;
         position.y += speed;
         break;
     }
-	changeDirectionTimer -= 1.0f;
+	changeDirectionTimer -= GetFrameTime();
 }
 
 
